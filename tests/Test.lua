b959ac4f1db6862dc9b087c2a59ba44a2fefc219
Kevin Higgins <Kevin.Higgins@biworldwide.com>
2015-05-27 12:14:53 -0500

    Enh: More work dnoe on parsing a log file into the in-game event format

--- tests/Test.lua
+++ tests/Test.lua
@@ -1,4 +1,4 @@
-package.path = ";../?.lua" .. package.path
+package.path = package.path .. ";../?.lua"
 
 require('inspect')
 require('lib/ParseCSVLine')
@@ -12,32 +12,77 @@ local line = io.read()
 
 object = {}
 
-while (line ~= nil) do
+function string.starts(String, Start)
+    return string.sub(String, 1, string.len(Start)) == Start
+end
 
-    -- local date, time, data = string.gmatch(line, "%S+");
+-- TODO pull this code into a wow LOG Parser class/lib
+function parseEventData(data)
 
-    tokens = {}
-    for i in string.gmatch(line, "%S+") do
-        tokens[#tokens + 1] = i
+    local eventName = data[1]
+    local eventData = {}
+
+    local isSpell = eventName:starts("SPELL_") or eventName:starts("RANGE_")
+    local isSwing = eventName:starts("SWING_")
+    local isEnvironmental = eventName:starts("ENVIRONMENTAL_")
+
+    -- TODO parse ENCOUNTER_START/ENCOUNTER_END events
+    -- TODO find out if those columns with zeroes in them are what gets populated with advanced logging.
+
+    -- Parse these events below - SWING, RANGE, SPELL, SPELL_PERIODIC, SPELL_BUILDING, ENVIRONMENTAL
+    if isSpell or isSwing or isEnvironmental then
+        -- Parse out the generic event fields
+        for i = 1, 9, 1 do
+            eventData[i] = data[i]
+        end
+
+        if isSpell then
+            parseSpellData(data)
+        elseif isSwing then
+            parseSwingData(data)
+        elseif isEnvironmental then
+            parseEnvironmental(data)
+        else
+            error("It should not be possible to reach this statement.")
+        end
     end
+    os.exit()
+end
 
-    local date, time, data = unpack(tokens)
-    local datetime = date .." " .. time
+function parseSpellData(data)
+    print("IS_SPELL")
+end
 
-    local pattern = "(%d+)/(%d+) (%d+):(%d+):(%d+).(%d+)"
-    local month, day, hour, minute, second, millisecond = datetime:match(pattern)
+function parseSwingData(data)
+    print("IS_SWING")
+end
 
-    -- If this gets more complicated, the year should be stored and parsed out of the file name.
-    timestamp = os.time({year=2015, month=month, day=day, hour=hour, min=minute, sec=second})
+function parseEnvironmental(data)
+    print("IS_ENVIRONMENTAL")
+end
+
+
+while (line ~= nil) do
+    -- Extract the timestamp and combat data
+    local pattern = "(%d+)/(%d+) (%d+):(%d+):(%d+).(%d+)%s+(.*)"
+    local month, day, hour, minute, second, millisecond, data = line:match(pattern)
 
     local result = ParseCSVLine(data)
+    --local event = result[1]
 
-    local event = result[1]
+    parseEventData(result)
+
+
+
+    -- TODO If this gets more complicated, the year should be stored and parsed out of the file name.
+    timestamp = os.time({ year = 2015, month = month, day = day, hour = hour, min = minute, sec = second })
+
+    --    print(event)
 
     -- Parse the data into the correct data series based on the incoming event.
 
 
-    os.exit()
-    WhoDunIt:processCombatLog(object)
+    --    os.exit()
+    --    WhoDunIt:processCombatLog(object)
     line = io.read()
 end
