19e2ddbea36610e4fc32e1ebbedcec9e3c775991
Kevin Higgins <kev.higgins@gmail.com>
2015-05-30 21:51:52 -0500

    Bug: Fixed issue where molten torrent receivers were sometimes placed in the wrong torrent group

--- encounters/Kagraz/MoltenTorrent.lua
+++ encounters/Kagraz/MoltenTorrent.lua
@@ -14,27 +14,28 @@ local activeTorrentData
 -- TODO make sure being immune still splits damage
 function MoltenTorrent:processEventData(eventData)
     local eventName = eventData[2]
-    if eventName == "SPELL_AURA_REMOVED" then
+    -- TODO Bug SPELL_AURA_REMOVED sometimes happens after the damage
+    if eventName == "SPELL_CAST_SUCCESS" then
         -- Track the player who was originally targeted by molten torrent
         local spellId = tonumber(eventData[12])
         if spellId == SPELL_AURA_ID then
-            local timestamp = eventData[1]
             local guid = eventData[8]
             local name = eventData[9]
             local unit = WhoDunIt.Unit:generate(guid, name)
 
             -- Create a new torrent data item. We will add the hits to this as the come in.
-            activeTorrentData = { timestamp = timestamp, hits = {}, deaths = {} }
+            activeTorrentData = { hits = {}, deaths = {} }
             self:logFail({ unit }, activeTorrentData)
         end
     elseif eventName == "SPELL_DAMAGE" or eventName == "SPELL_MISSED" then
         -- Track all players who were hit by molten torrent
         local spellId = tonumber(eventData[12])
         if spellId == SPELL_ID then
+            local timestamp = eventData[1]
             local guid = eventData[8]
             local name = eventData[9]
             local unit = WhoDunIt.Unit:generate(guid, name)
-            table.insert(activeTorrentData.hits, { unit = unit })
+            table.insert(activeTorrentData.hits, { timestamp = timestamp, unit = unit })
         end
     elseif eventName == "UNIT_DIED" then
         -- If a unit that got hit by molten torrent died soon after, then mark the death.
@@ -44,14 +45,15 @@ function MoltenTorrent:processEventData(eventData)
         local unitWasHit = false
 
         if activeTorrentData ~= nill then
+            local torrentTimestamp
             for key, hit in pairs(activeTorrentData.hits) do
                 if hit.unit.guid == guid then
                     unitWasHit = true
+                    torrentTimestamp = hit.timestamp
                     break
                 end
             end
-            local torrentTimestamp = activeTorrentData.timestamp
-            if unitWasHit and timestamp - torrentTimestamp < TORRENT_DEATH_THRESHOLD then
+            if unitWasHit and math.abs(timestamp - torrentTimestamp) < TORRENT_DEATH_THRESHOLD then
                 local unit = WhoDunIt.Unit:generate(guid, name)
                 table.insert(activeTorrentData.deaths, unit)
             end
